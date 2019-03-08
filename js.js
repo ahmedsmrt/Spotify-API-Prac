3dea05546765b1bc892208516aba5ef6fb6a21d9
A.G.Sudani <36556422+ahmedsmrt@users.noreply.github.com>
2019-03-08 05:03:35 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+// 
+fetch('https://api.spotify.com/v1/search').then(response => {
+    return response.json();
+}).catch(err => {
+    console.log("There was an Error Bruh -___-");
+})
+
+
+
+// UIController
+
+
+
+
+
+
+// Global App Controller
+
+
+
